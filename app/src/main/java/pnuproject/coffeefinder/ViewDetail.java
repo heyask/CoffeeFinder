d899e51f1de816eda31dee5ecc4c2a2c56f9c347
heyask <sunrivs@gmail.com>
2017-06-10 18:58:28 +0900

    1. 메인지도 완성 2. 상세페이지 제작중

--- app/src/main/java/pnuproject/coffeefinder/ViewDetail.java
+++ app/src/main/java/pnuproject/coffeefinder/ViewDetail.java
@@ -1,54 +1,27 @@
 package pnuproject.coffeefinder;
 
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.view.ViewCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
-import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
-import android.text.style.StyleSpan;
-import android.transition.Slide;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.R.id.primary;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-
 public class ViewDetail extends AppCompatActivity {
-    class Entry
-    {
-        int value;
-    }
+    PieChart pieChart;
+    PieDataSet pieDataSet;
+    PieData pieData;
 
-    @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,71 +31,33 @@ public class ViewDetail extends AppCompatActivity {
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("모카 프라푸치노 - 스타벅스");
 
-        /*final ImageView image = (ImageView) findViewById(R.id.image);
-        Picasso.with(this).load(getIntent().getStringExtra(EXTRA_IMAGE)).into(image, new Callback() {
-            @Override public void onSuccess() {
-                Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
-                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-                    public void onGenerated(Palette palette) {
-                        applyPalette(palette);
-                    }
-                });
-            }
-
-            @Override public void onError() {
-
-            }
-        });*/
-
-        /*TextView title = (TextView) findViewById(R.id.title);
-        title.setText(itemTitle);*/
-
-        //CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        //collapsingToolbar.setTitle("Title");
-
-        PieChart pieChart = (PieChart) findViewById(R.id.chart_rating);
-        pieChart.getDescription().setEnabled(false);
-        pieChart.getLegend().setEnabled(false);
-        //YourData[] dataObjects = ...;
-
-        //List<PieEntry> entries = new ArrayList<>();
-        //entries.add(new PieEntry(60f, "긍정"));
-        //entries.add(new PieEntry(40f, "부정"));
-
-        //PieDataSet set = new PieDataSet(entries, "Election Results");
-        //PieData data = new PieData(set);
-        //pieChart.setData(data);
-        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
-        entries.add(new PieEntry(60f, "호"));
-        entries.add(new PieEntry(40f, "불호"));
-
-        PieDataSet dataSet = new PieDataSet(entries, "긍정,부정 리뷰 비율");
-        dataSet.setDrawIcons(false);
-
-        //dataSet.setSliceSpace(3f);
-        //dataSet.setIconsOffset(new MPPointF(0, 40));
-        dataSet.setSelectionShift(5f);
-        List<Integer> colors = new ArrayList<Integer>();
-
-        colors.add(Color.rgb(111, 255, 0));
-        colors.add(Color.rgb(255, 0, 64));
-
-        dataSet.setColors(colors);
-        //dataSet.setSelectionShift(0f);
-
-        PieData data = new PieData(dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.BLACK);
-        //data.setValueTypeface(mTfLight);
-        pieChart.setData(data);
-        //pieChart.setTextColor(Color.rgb(0, 0, 0));
-
+        pieChart = (PieChart) findViewById(R.id.chart_rating);
         pieChart.setEntryLabelColor(Color.BLACK);
         pieChart.setEntryLabelTextSize(12f);
-
-        pieChart.invalidate(); // refresh
         pieChart.setTouchEnabled(false);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.getLegend().setEnabled(false);
+
+        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
+        entries.add(new PieEntry(60f, "긍정"));
+        entries.add(new PieEntry(40f, "부정"));
+
+        pieDataSet = new PieDataSet(entries, "긍정,부정 리뷰 비율");
+        pieDataSet.setDrawIcons(false);
+        pieDataSet.setSelectionShift(5f);
+
+        List<Integer> colors = new ArrayList<Integer>();
+        colors.add(Color.rgb(111, 255, 0));
+        colors.add(Color.rgb(255, 0, 64));
+        pieDataSet.setColors(colors);
+
+        pieData = new PieData(pieDataSet);
+        pieData.setValueFormatter(new PercentFormatter());
+        pieData.setValueTextSize(11f);
+        pieData.setValueTextColor(Color.BLACK);
+        pieChart.setData(pieData);
+
+        pieChart.invalidate();
 
         pieChart.setCenterText(generateCenterSpannableText());
     }
